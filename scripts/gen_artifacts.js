ec5f42b03b7690a36f375874242d51c10136ba00
Savio <72797635+Savio-Sou@users.noreply.github.com>
2023-11-19 01:51:57 +0300

    Add TODOs & graceful end of script

--- scripts/gen_artifacts.js
+++ scripts/gen_artifacts.js
@@ -6,27 +6,7 @@ import * as fs from 'fs';
 const backend = new BarretenbergBackend(circuit);
 const noir = new Noir(circuit, backend);
 
-async function main(inputs) {
-    const { witness } = await noir.execute(inputs)
-    const proof = await backend.generateIntermediateProof(witness);
-    const { proofAsFields, vkHash, vkAsFields } = await backend.generateIntermediateProofArtifacts(
-        proof,
-        4
-    );
-
-    let artifacts = {
-        'proofAsFields: ': proofAsFields,
-        'vkAsFields: ': vkAsFields,
-        'vkHash: ': vkHash
-    }
-
-    // Convert the artifacts to a JSON string
-    const jsonString = JSON.stringify(artifacts, null, 2);
-
-    // Write the JSON string to the file
-    fs.writeFileSync('./scripts/out/artifacts.json', jsonString);
-}
-
+// TODO: Generalize the circuit and this
 const inputs = {
     hash_path: [
     "0x2f402b1f15a72cbab7f1ac7b405516d3bf8b8b3523d195e317ee8a2cb439c6d0",
@@ -37,6 +17,33 @@ const inputs = {
     root : "0x24efd4b26f3cac900b34fb1e141a54cb1aa27213f4ff95abe8290b4b6813ae4b",
     secret  : ["1", "10"],
     vote : "1"
+};
+
+async function main(inputs) {
+    try {
+        const { witness } = await noir.execute(inputs);
+        const proof = await backend.generateIntermediateProof(witness);
+        const { proofAsFields, vkHash, vkAsFields } = await backend.generateIntermediateProofArtifacts(proof, 4);
+
+        let artifacts = {
+            'proofAsFields: ': proofAsFields,
+            'vkAsFields: ': vkAsFields,
+            'vkHash: ': vkHash
+        };
+
+        // Convert the artifacts to a JSON string
+        const jsonString = JSON.stringify(artifacts, null, 2);
+
+        // Write the JSON string to the file
+        fs.writeFileSync('./scripts/out/artifacts.json', jsonString);
+
+        console.log('File written successfully.');
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        // Optionally, you can use process.exit() to forcefully exit the script
+        process.exit();
+    }
 }
 
-main(inputs)
+main(inputs);
